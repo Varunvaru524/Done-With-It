1d708ddfc576c2ea968ce578005a10ff14f1b3d9
Varun <varunvaru524@gmail.com>
2023-03-26 22:00:43 +0530

    Rearranged all the files and createed welcome page

---
+++ src/Pages/WelcomePage.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function WelcomePage(props) {
+    return ( <Text>Welcome Screen</Text>  );
+}
+
+export default WelcomePage;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import WelcomePage from './Pages/WelcomePage';
+
+class Index extends Component {
+    render() { 
+        return (<WelcomePage/>);
+    }
+}
+ 
+export default Index;
