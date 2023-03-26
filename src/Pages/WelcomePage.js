1c119c36250bd6452addc834a9e1fd19c638446f
Varun <varunvaru524@gmail.com>
2023-03-26 23:33:25 +0530

    Updated welcome page

--- src/Pages/WelcomePage.js
+++ src/Pages/WelcomePage.js
@@ -1,12 +1,47 @@
-import { Text, ImageBackground, View } from "react-native";
+import { Text, ImageBackground, View, StyleSheet, Image } from "react-native";
 import backgroundImage from '../assets/background.jpg'
+import Logo from '../assets/logo-red.png'
+
 
 function WelcomePage(props) {
     return (
-        <View>
-            <ImageBackground style={{  height: "100%" }} source={backgroundImage} />
-        </View>
+        <ImageBackground style={styles.bgImage} source={backgroundImage}>
+            <View style={styles.loginButton} />
+            <View style={styles.registerButton} />
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={Logo} />
+                <Text>Sell What You Don't Need</Text>
+            </View>
+        </ImageBackground>
     );
 }
+
+const styles = StyleSheet.create({
+    bgImage: {
+        height: "100%",
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    loginButton: {
+        height: 70,
+        backgroundColor: '#fc5c65',
+    },
+    registerButton: {
+        height: 70,
+        backgroundColor: '#4ecdc4',
+    },
+    logoContainer:{
+        position: "absolute",
+        top: 100,
+        width:"100%",
+        display:"flex",
+        alignItems:"center"
+    },
+    logo: {
+        height: 100,
+        width: 100,
+    }
+})
+
 
 export default WelcomePage;
