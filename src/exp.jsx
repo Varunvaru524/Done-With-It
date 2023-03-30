0dcfc76f3f7763084444e1f7882972d5989cb208
Varun <varunvaru524@gmail.com>
2023-03-30 11:59:29 +0530

    Created Icon component

--- src/exp.jsx
+++ src/exp.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Img from './assets/jacket.jpg'
-import MessagesPage from './Pages/MessagesPage';
+import PageLayout from './Components/PageLayout';
+import Icon from './Components/Icon';
 
 class Experimental extends Component {
     render() { 
-        return (<MessagesPage />);
+        return (
+            <PageLayout>
+                <Icon />
+            </PageLayout>
+        );
     }
 }
 
