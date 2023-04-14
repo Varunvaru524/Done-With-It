15b510c713cd302fa91948453626361956665bfc
Varun <varunvaru524@gmail.com>
2023-04-15 01:14:33 +0530

    Just testing navigation

--- src/exp.jsx
+++ src/exp.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
-import LoginPage from './Pages/LoginPage';
+import { Button, Text, View } from 'react-native';
+import PageLayout from './Components/PageLayout';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+
+function Tweet(props) {
+    return (
+        <View>
+            <Text>Tweets</Text>
+        </View>
+    );
+}
+
+function TweetDetails({navigation}) {
+    return (
+        <View>
+            <Text>TweetsDetails</Text>
+            <Button title='Click Me' onPress={()=>navigation.navigate('Tweets')}/>
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
+
+
+
+
+// let TheNavigetor = (props) => {
+//     return (
+//         <Stack.Navigator>
+//             <Stack.Screen name='Tweets' component={Tweet} />
+//         </Stack.Navigator>
+//     )
+// }
+
+
 
 class Experimental extends Component {
-    state = {
-        valueChange: false
-    }
+
     render() {
         return (
-            <LoginPage/>
+            <PageLayout>
+                <NavigationContainer>
+                    <Stack.Navigator initialRouteName='TweetDetails'>
+                        <Stack.Screen name='Tweets' component={Tweet} />
+                        <Stack.Screen name='TweetDetails' component={TweetDetails} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </PageLayout>
         );
     }
 }
