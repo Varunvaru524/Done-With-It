18e10c7fcf1d4b7135b6975ad5e2c4540e52a6e7
Varun <varunvaru524@gmail.com>
2023-05-04 16:11:51 +0530

    Added image picker functionality to new post page

--- src/Pages/NewPostPage.jsx
+++ src/Pages/NewPostPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableWithoutFeedback,Image, ScrollView } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import * as imagePicker from 'expo-image-picker'
 import rootCss from '../rootCss';
 import PageLayout from '../Components/PageLayout';
 import AppTextInput from '../Components/AppTextInput';
@@ -8,15 +9,50 @@ import AppButton from '../Components/AppButton';
 
 
 class NewPostPage extends Component {
+  state = {
+    permission: null,
+    uri: []
+  }
+
+  async handleImages() {
+    const permission = await imagePicker.getMediaLibraryPermissionsAsync().catch(reject => console.log('Get permission', reject))
+    this.setState({ permission: permission.granted })
+
+    const selectImages = async () => {
+      const images = await imagePicker.launchImageLibraryAsync().catch(reject => console.log(reject))
+      const updatedUri = this.state.uri
+      updatedUri.unshift(images.assets[0].uri)
+      this.setState({ uri: updatedUri })
+    }
+
+    if (this.state.permission) {
+      selectImages()
+    }
+    else {
+      const permission = await imagePicker.requestMediaLibraryPermissionsAsync().catch(reject => console.log('Request permission', reject))
+      this.setState({ permission: permission.granted })
+      selectImages()
+    }
+  }
+
   render() {
     return (
       <PageLayout>
         <View style={styles.container}>
           <Text style={styles.title}>New Post</Text>
           <View style={styles.imagesContainer}>
-            <View style={styles.eachImage}>
-              <MaterialCommunityIcons name='camera' size={40} color={rootCss.mediumGrey} />
-            </View>
+            <ScrollView horizontal>
+              <TouchableWithoutFeedback onPress={() => this.handleImages()}>
+                <View style={styles.eachImage}>
+                  <MaterialCommunityIcons name='camera' size={40} color={rootCss.mediumGrey} />
+                </View>
+              </TouchableWithoutFeedback>
+              {this.state.uri.map(item => (
+                <View style={styles.eachImage} key={item}>
+                  <Image source={{ uri: item, width: 100, height: 100 }} />
+                </View>
+              ))}
+            </ScrollView>
           </View>
           <AppTextInput icon='lead-pencil' placeholder='Title' />
           <AppTextInput icon='currency-rupee' placeholder='Price' />
@@ -33,21 +69,22 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 10
   },
-  imagesContainer: {
-
-  },
+  imagesContainer: {},
   eachImage: {
     backgroundColor: rootCss.white,
     height: 100,
     width: 100,
     borderRadius: 20,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginRight: 10,
+    overflow: 'hidden'
   },
   title: {
     fontSize: 25,
     fontWeight: '600',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginBottom: 10
   },
   cancel: {
     fontSize: 18,
